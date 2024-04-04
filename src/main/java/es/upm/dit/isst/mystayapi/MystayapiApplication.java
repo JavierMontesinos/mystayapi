9f30d2643e788dc3ae018bf0afc5eaa281d1ae82
JavierMontesinos <j.montesinos@alumnos.upm.es>
2024-04-04 09:46:04 +0200

    Version Inicial del proyetcto

---
+++ src/main/java/es/upm/dit/isst/mystayapi/MystayapiApplication.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.mystayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystayapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystayapiApplication.class, args);
+	}
+
+}
