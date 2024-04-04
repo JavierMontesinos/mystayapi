a03158c1934e9c029499af86584e7987d69d1604
JavierMontesinos <j.montesinos@alumnos.upm.es>
2024-04-04 17:17:47 +0200

    Version Inicial del Backend

---
+++ src/main/java/es/upm/dit/isst/mystayapi/model/Cliente.java
@@ -0,0 +1,37 @@
+package es.upm.dit.isst.mystayapi.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idcliente")
+    private Integer id;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    @Column(nullable = false, unique=true)
+    private String dni;
+
+    private Integer Nhab;
+
+    private boolean Premium;
+
+    private Integer Gasto;
+    
+    private String Pagado;
+
+
+
+}
+    
