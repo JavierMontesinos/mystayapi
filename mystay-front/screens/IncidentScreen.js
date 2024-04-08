25f9ee9ab66dad9dc34388b5b20ff1cabb86864e
JavierMontesinos <j.montesinos@alumnos.upm.es>
2024-04-08 13:42:59 +0200

    Incidencias Completadas

--- mystay-front/screens/IncidentScreen.js
+++ mystay-front/screens/IncidentScreen.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 
-const IncidentScreen = () => {
+const QuestionnaireScreen = ({ navigation }) => {
+  const [incidentText, setIncidentText] = useState('');
+
+  const handleSubmit = () => {
+    alert(`Incidencia enviada con éxito`);
+    setIncidentText('');
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to the Incident Screen!</Text>
+    <View style={styles.container}>
+      <Text>Escribe tu incidencia:</Text>
+      <TextInput
+        style={styles.input}
+        multiline
+        numberOfLines={4}
+        value={incidentText}
+        onChangeText={(text) => setIncidentText(text)}
+      />
+      <Button title="Enviar Incidencia" onPress={handleSubmit} />
     </View>
   );
 };
 
-export default IncidentScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginTop: 10,
+    marginBottom: 20,
+    height: 100,
+  },
+});
+
+export default QuestionnaireScreen;
